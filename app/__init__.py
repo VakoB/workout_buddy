e027192a17a185f25d25d4bcc24584f9014c0b50
VakoB <vladimer.butkhuzi.1@btu.edu.ge>
2024-04-30 00:10:48 +0400

    used User model, sqlite3, registering, login user (without validation)

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,29 @@
 from flask import Flask
+from app.extensions import db, jwt
+import os
+from dotenv import load_dotenv
+
 
 
 def create_app():
     app = Flask(__name__)
+
     from .auth import authBP
     from .views import viewsBP
 
+    load_dotenv()
+    app.config.from_prefixed_env()
+
+
+    #initializing extensions
+    db.init_app(app)
+    jwt.init_app(app)
+
+    
     app.register_blueprint(authBP, url_prefix="/auth")
     app.register_blueprint(viewsBP)
 
+    
     # ...
     
 
--- app/auth.py
+++ app/auth.py
@@ -1,11 +1,64 @@
-from flask import Flask, request, Blueprint, render_template
+from flask import Flask, request, Blueprint, render_template, jsonify
+from app.models import User
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 authBP = Blueprint('auth', __name__)
 
 @authBP.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('auth/login.html')
+    if request.method == 'GET':
+        return render_template('auth/login.html')
+    elif request.method == 'POST':
+            
+        data = request.get_json()
+
+        user = User.get_user_by_username(username=data.get('username'))
+
+        if user and (user.check_password(password=data.get('password'))):
+
+            access_token = create_access_token(identity=user.username)
+            refresh_token = create_refresh_token(identity=user.username)
+            return jsonify(
+                {
+                    "message": "Logged in",
+                    "tokens": {
+                        "access": access_token,
+                        "refresh": refresh_token
+                    }
+                }, 200
+            )
+        return jsonify(
+            {
+                "error": "Invalid username or password."
+            }, 400
+        )
 
 @authBP.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template('auth/register.html')
+
+    if request.method == 'GET':
+        return render_template('auth/register.html')
+    elif request.method == 'POST':
+
+        data = request.get_json()
+        user = User.get_user_by_username(username=data.get('username'))
+
+        if user is None:
+            new_user = User(
+                username = data.get('username'),
+                email = data.get('email')
+            )
+            new_user.set_password(data.get('password'))
+
+            new_user.save()
+            print('user created')
+            # redirect to the main page (or sign in page)
+            return jsonify({'suceess': 'the user was created.'}), 201
+            
+
+        
+        elif user is not None:
+            return jsonify({'error': 'the user already exists.'}), 403
+        
+
+    
